edf35181dfaaf0acf1b347c4bd4906acb762a011
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-06-04 21:50:31 +0100

    Update CardContato.js

--- components/contato/CardContato.js
+++ components/contato/CardContato.js
@@ -5,7 +5,7 @@ export default function CardContato() {
   return (
     <>
       <div className="bg-white flex flex-col shadow-md justify-center items-center text-gray-600 space-y-4 py-3 border">
-        <AiOutlineContacts size="4em" color='#4b991d' />
+        <AiOutlineContacts size="80px" color='#4b991d' />
         <p className='text-xl'>+55 71 123 456 789</p>
         <p className=''>Mutuípe, Bahia, Brazil</p>
         <p className=' tracking-wider'>contato@agrobahiaatacadista.com.br</p>
